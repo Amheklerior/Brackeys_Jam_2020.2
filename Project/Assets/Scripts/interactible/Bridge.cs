e2bdbeebb982f40cd6f388bfbbd2bfc21275954c
amato_davide <amatodavide1@gmail.com>
2020-08-05 21:29:35 +0200

    Prefab Level_2 created. TODO Implement logic

---
+++ Project/Assets/Scripts/interactible/Bridge.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace Amheklerior.Rewind
+{
+    public class Bridge : MonoBehaviour
+    {
+        private Quaternion originalRot;
+        public void MoveDown()
+        {
+            originalRot = transform.rotation;
+            transform.rotation = Quaternion.identity;
+        }
+        public void MoveUp()
+        {
+            transform.rotation = originalRot;
+        }
+    }
+}
