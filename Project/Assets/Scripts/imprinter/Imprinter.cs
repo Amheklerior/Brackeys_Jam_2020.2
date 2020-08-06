969ae8db7cf73d16f8a4405e79d319761d441bef
Andrea <andrea.amato.89@gmail.com>
2020-08-06 21:09:05 +0100

    Adds more sounds

--- Project/Assets/Scripts/imprinter/Imprinter.cs
+++ Project/Assets/Scripts/imprinter/Imprinter.cs
@@ -2,26 +2,39 @@
 
 namespace Amheklerior.Rewind {
     
+    [RequireComponent(typeof(AudioSource))]
     public class Imprinter : MonoBehaviour {
         
         [Header("Dependencies:")]
         [SerializeField] private PlayerState _player;
+        [SerializeField] private AudioEvent _giveImprintSound;
+        [SerializeField] private AudioEvent _removeImprintSound;
+        [SerializeField] private AudioEvent _invalidSound;
 
         [Space]
         [Header("Settings:")]
         [SerializeField] private Imprint _type;
 
+        private AudioSource _source;
+
         private void Awake() {
             if (_type == Imprint.NONE) Debug.LogError("The imprinter cannot be of type NONE.", this); 
             if (_player == null) Debug.LogError("The player state ref is not set.", this);
+            _source = GetComponent<AudioSource>();
         }
 
         private void OnTriggerEnter(Collider other) {
-            if (!_player.HasImprint && !_player.IsRewinding)
+            if (!_player.HasImprint && !_player.IsRewinding) {
                 _player.MarkWith(_type);
+                _giveImprintSound.Play(_source);
 
-            else if (_player.IsMarkedWith(_type) && _player.IsRewinding)
+            } else if (_player.IsMarkedWith(_type) && _player.IsRewinding) {
                 _player.RemoveMark();
+                _removeImprintSound.Play(_source);
+
+            } else {
+                _invalidSound.Play(_source);
+            }
         }
     }
 }
--- Project/Assets/Scripts/interactible/InteractionHandler.cs
+++ Project/Assets/Scripts/interactible/InteractionHandler.cs
@@ -7,6 +7,7 @@ namespace Amheklerior.Rewind {
 
         [Header("Dependencies:")]
         [SerializeField] private PlayerState _player;
+        [SerializeField] private AudioEvent _interactSound;
 
         [Space]
         [Header("Settings:")]
@@ -14,16 +15,19 @@ namespace Amheklerior.Rewind {
         [SerializeField] private UnityEvent _onInteract;
 
         private bool _alreadyInteracted;
+        private AudioSource _source;
 
         private void Awake() {
             if (_player == null) Debug.LogError("The player state ref is not set.", this);
             if (_requiredImprint == Imprint.NONE)
                 Debug.LogError("The interactible object has to require an imprint other than NONE.", this);
+            _source = GetComponent<AudioSource>();
         }
 
         private void OnTriggerEnter(Collider other) {
             if (!_player.IsMarkedWith(_requiredImprint) || _alreadyInteracted) return;
             _alreadyInteracted = true;
+            _interactSound.Play(_source);
             _onInteract.Invoke();
         }
         
