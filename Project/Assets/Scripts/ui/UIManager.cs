55fc74c8f877a5db51a27099e3ea9269a98c27c0
amato_davide <amatodavide1@gmail.com>
2020-08-07 20:07:13 +0200

    new UI integrated

--- Project/Assets/Scripts/ui/UIManager.cs
+++ Project/Assets/Scripts/ui/UIManager.cs
@@ -5,16 +5,28 @@ namespace Amheklerior.Rewind {
 
     public class UIManager : MonoBehaviour {
 
-        [SerializeField] private GameObject _cubeTitle;
-
         [Header("Game events:")]
         [SerializeField] private GameEvent _startNewGame;
 
+        [Header("UI refs:")]
+        [SerializeField] private GameObject _mainmenuCanvas;
+        [SerializeField] private GameObject _controlsCanvas;
+
         public void PlayButtonClicked()
         {
-            //_cubeTitle.SetActive(false);
             _startNewGame.Raise();
         }
 
+        public void ControlsButtonClicked()
+        {
+            _mainmenuCanvas.SetActive(false);
+            _controlsCanvas.SetActive(true);
+        }
+
+        public void BackBtnControlsClicked()
+        {
+            _controlsCanvas.SetActive(false);
+            _mainmenuCanvas.SetActive(true);
+        }
     }
 }
