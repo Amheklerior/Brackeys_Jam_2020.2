cad3b2fc107901feab3150a36e56e595c12de660
amato_davide <amatodavide1@gmail.com>
2020-08-07 19:02:34 +0200

    UI: new ui prefab created (menu,controls,victory)

--- Project/Assets/Scripts/ui/UIManager.cs
+++ Project/Assets/Scripts/ui/UIManager.cs
@@ -4,11 +4,16 @@ using Amheklerior.Core.EventSystem;
 namespace Amheklerior.Rewind {
 
     public class UIManager : MonoBehaviour {
+        [SerializeField] private GameObject _cubeTitle;
 
         [Header("Game events:")]
         [SerializeField] private GameEvent _startNewGame;
-        
-        public void PlayButtonClicked() => _startNewGame.Raise();
+
+        public void PlayButtonClicked()
+        {
+            _cubeTitle.SetActive(false);
+            _startNewGame.Raise();
+        }
 
     }
 }
